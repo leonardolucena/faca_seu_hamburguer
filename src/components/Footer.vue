3c492bc59f530b9d4064bd1b233033d202b90edc
Leonardo Lucena <leo.lucena10@hotmail.com>
2022-11-13 15:46:02 -0300

    alteração nos componentes

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Make your burger &copy; 2021</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        name: 'Footer'
+    }
+
+</script>
+
+<style scoped>
+    #footer {
+        height: 150px;
+        background-color: #222;
+        border-top: #111;
+        color: #fcba03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+</style>
